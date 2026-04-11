e2bd13831083e1fefc93eb10d96cd37879587638
Codons-ensemble <bounamakountagoudiaby@gmail.com>
2026-04-11 16:07:25 +0100

    first commit HOST

--- authentification/settings.py
+++ authentification/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = "django-insecure-_k02r5xyf^metsz!*=f#7tit8y#y$x&s52*15(1&b^j3l(g-#y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["ecsisarl-backed.onrender.com",
+                 "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ['https://ecsisarl-backed.onrender.com',
+                        'https://ecsisarl-backed.onrender.com',]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Application definition
@@ -58,9 +63,15 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 ]
 
+
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
+    "https://ecsisarl-backed.onrender.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
+
+
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
